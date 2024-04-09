0984be510c0821425ed4f596e5ff32c3a2aa066f
calenzo <calenzodevs@gmail.com>
2024-04-09 13:23:10 -0300

    fix: docker

--- app/database/settings.py
+++ app/database/settings.py
@@ -7,16 +7,8 @@ from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 
-USERNAME = os.environ["DB_USERNAME"]
-HOSTNAME = os.environ["DB_HOSTNAME"]
-PASSWORD = os.environ["DB_PASSWORD"]
-DATABASE = os.environ["DB_DATABASE"]
-DIALECT = os.environ["DB_DIALECT"]
-PORT = os.environ["DB_PORT"]
 
-
-SQLALCHEMY_DATABASE_URL = \
-    f'{DIALECT}://{USERNAME}:{PASSWORD}@{HOSTNAME}:{PORT}/{DATABASE}'
+SQLALCHEMY_DATABASE_URL = os.environ["DATABASE_URL"]
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 Base = declarative_base()
