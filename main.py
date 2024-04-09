eb430eebcbfdf101266ddf772af3425be0ed98a6
calenzo <calenzodevs@gmail.com>
2024-04-09 15:01:09 -0300

    fix: i was added the filter of erro

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.database.settings import create_tables
+from app.interactors.response_api_interactor import ResponseError
 from app.routes import (
     user_routes,
     link_routes,
 )
 
+
 create_tables()
 
 app = FastAPI()
+
+
+@app.exception_handler(ResponseError)
+async def unicorn_exception_handler(request: Request, exc: ResponseError):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content=exc.error(),
+    )
 
 app.add_middleware(
     CORSMiddleware,
